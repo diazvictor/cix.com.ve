a474d90a04a2391347234f2d3d14eac3a0f84230
diazvictor <diaz.victor@openmailbox.org>
2019-11-10 19:21:59 -0400

    Completado el slider de la pagina, algunas fuentes innecesarias han sido borradas y otras añadidas

--- js/main.js
+++ js/main.js
@@ -13,9 +13,14 @@ var myFullpage = new fullpage('#fullpage',{
     // ──────────────────────────────────────────────────
     //   :::::: Barra de navegación
     // ──────────────────────────────────────────────────
-    navigation: true, // Muesta la barra de navegación.
+    navigation: false, // Muesta la barra de navegación.
     menu: '#menu', // Menu de navegación.
-    anchors: ['inicio', 'servicios', 'contactos'], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
-    navigationTooltips: ['Inicio', 'Servicios', 'Contactos'], // Tooltips que mostrara por cada boton.
+    anchors: ['inicio', 'quienesomos', 'servicios', 'solucionesit', 'contactos'], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
+    navigationTooltips: ['Inicio', '¿ Quienes Somos ?', 'Servicios', 'Soluciones IT', 'Contactos'], // Tooltips que mostrara por cada boton.
     showActiveTooltip: false, // Mostrar tooltip activa.
+	// ──────────────────────────────────────────────────
+    //   :::::: Secciones
+    // ──────────────────────────────────────────────────
+    sectionsColor: ['#000', '#c2c2c2', '#000', '#000', '#000'], // Color de fondo de cada seccion.
+    verticalCentered: true, // Si alineara de forma vertical los contenidos de cada seccion.
 });
