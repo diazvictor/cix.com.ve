e5864bd716f8ac668ef595f31ae7bd881db73681
diazvictor <diaz.victor@openmailbox.org>
2019-11-10 17:23:40 -0400

    Completado el header de la pagina, algunas fuentes innecesarias han sido borradas y otras añadidas

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 var myFullpage = new fullpage('#fullpage',{
-	
+	// ──────────────────────────────────────────────────
+    //   :::::: Opciones Basicas
+    // ──────────────────────────────────────────────────
+    autoScrolling: true, // Se activa el scroll.
+    fitToSection: false, // Acomoda el scroll automaticamente para que la seccion se muestre en pantalla.
+    fitToSectionDelay: 300, // Delay antes de acomodar la seccion automaticamente.
+    easing: 'easeInOutCubic', // Funcion de tiempo de la animacion.
+    scrollingSpeed: 700, // Velocidad del scroll. Valores: en milisegundos.
+    css3: true, // Si usara CSS3 o javascript.
+    easingcss3: 'ease-out', // Curva de velocidad del efecto.
+    loopBottom: false, // Regresa a la primera seccion siempre y cuando se ya haya llegado a la ultima sección y el ususario siga scrolleando.
+    // ──────────────────────────────────────────────────
+    //   :::::: Barra de navegación
+    // ──────────────────────────────────────────────────
+    navigation: true, // Muesta la barra de navegación.
+    menu: '#menu', // Menu de navegación.
+    anchors: ['inicio', 'servicios', 'contactos'], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
+    navigationTooltips: ['Inicio', 'Servicios', 'Contactos'], // Tooltips que mostrara por cada boton.
+    showActiveTooltip: false, // Mostrar tooltip activa.
 });
