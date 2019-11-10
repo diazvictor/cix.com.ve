13b9f250c8bb4c9c8c85f7f25b9e3ac245dece0d
diazvictor <diaz.victor@openmailbox.org>
2019-11-10 19:52:00 -0400

    Completado la sección de redes sociales de la pagina

--- js/main.js
+++ js/main.js
@@ -23,4 +23,15 @@ var myFullpage = new fullpage('#fullpage',{
     // ──────────────────────────────────────────────────
     sectionsColor: ['#000', '#c2c2c2', '#000', '#000', '#000'], // Color de fondo de cada seccion.
     verticalCentered: true, // Si alineara de forma vertical los contenidos de cada seccion.
+    // ──────────────────────────────────────────────────
+    //   :::::: Slides
+    // ──────────────────────────────────────────────────
+    controlArrows: true, // Flechas del slide
+    slidesNavigation: false, // Indicadores del slide
+    
+    afterLoad: function(origin, destination) {
+        if (destination.anchor == 'contactos') {
+            document.querySelector('.contactos').querySelector('h2').style.opacity = 1;
+        }
+    }
 });
